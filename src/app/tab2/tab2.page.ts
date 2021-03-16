e772ce4e83019f78c85ba032b07ba73c3dc264f9
Jessica <c14180106@john.petra.ac.id>
2021-03-16 20:16:02 +0700

    gameDone

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -3,10 +3,26 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
-  styleUrls: ['tab2.page.scss']
+  styleUrls: ['tab2.page.scss'],
 })
 export class Tab2Page {
+  number = 0;
+  userNumber = 0;
+  match = false;
 
   constructor() {}
 
+  ngOnInit() {
+    this.number = Math.floor(Math.random() * 9) + 1;
+    console.log(this.number);
+  }
+
+  check() {
+    if (this.number == this.userNumber) {
+      this.match = true;
+      console.log(this.match);
+    } else {
+      console.log(this.match);
+    }
+  }
 }
