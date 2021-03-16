19e694562a4c5209dc1a41dccdee8bfb80177809
Jessica <c14180106@john.petra.ac.id>
2021-03-16 19:11:43 +0700

    galeriDone

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FotoService } from '../services/foto.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  constructor(public fotoService: FotoService) {}
 
-  constructor() {}
+  async ngOnInit() {
+    await this.fotoService.loadFoto();
+  }
 
+  TambahFoto() {
+    this.fotoService.tambahFoto();
+  }
 }
